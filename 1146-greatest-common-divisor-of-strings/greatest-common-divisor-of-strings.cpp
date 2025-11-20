c22b76a4b6d0d89ca50f714365ca3db0a10633dd
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2025-11-20 19:28:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (59.00%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if((str1+str2) != (str2+str1)){
-            return "";
-        }else{
-            int m = str1.length();
-            int n = str2.length();
-            int l = gcd(m,n);
-            string result;
-            for(int i = 0;i<l;i++){
-                result.push_back(str1[i]);
-            }
+        string result;
+        if((str1+str2) == (str2+str1)){
+            int n = gcd(str1.length(),str2.length());
+            result = str1.substr(0,n);
             return result;
+        }else{
+            return "";
         }
+
     }
 };
