29a8787ba0f8ad183e666a0d1a6520aeeaf33239
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-01-06 15:49:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (49.42%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,20 +3,18 @@ public:
     int mySqrt(int x) {
         if( x == 0)return 0;
         if( x == 1)return 1;
-        int l = 1,r = x;
-        int ans = 0;
-        
+        int l = 1,r = x;   
         while(l<=r){
             int m = l+(r-l)/2;
-            if((long long)m*m == x){
+            long long s = (long long)m*m ;
+            if(s== x){
                 return m;
-            }else if((long long)m*m < x){
-                ans = m;
-                l = m+1;
-            }else{
+            }else if(s > x){
                 r = m-1;
+            }else{
+                l = m+1;
             }
         }
-        return ans;
+        return r;
     }
 };
