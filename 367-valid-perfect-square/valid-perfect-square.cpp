97a552d16186f7b71eaa82dad69ecfcdd047dcfd
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-02-11 22:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (52.71%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.cpp
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -3,7 +3,7 @@ public:
     bool isPerfectSquare(int num) {
         if(num == 1)return true;
         bool ans = false;
-        for(long int i = 2;i<num;i++){
+        for(long int i = 2;num>=i*i;i++){
             if(i*i == num){
                 ans = true;
                 break;
