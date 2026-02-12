0b72ab1fd114a4a5cf9ec6afe04caad9838a236e
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-02-12 23:26:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (16.25%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> result;
-        for(int i = 1;i<=n;i++){
+        for(int i = 1;i<n+1;i++){
             if(i%3 == 0 && i%5 == 0){
                 result.push_back("FizzBuzz");
                 
