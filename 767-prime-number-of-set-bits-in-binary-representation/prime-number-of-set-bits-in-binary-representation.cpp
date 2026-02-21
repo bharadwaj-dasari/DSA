3d01907c223919695d1af0ec5853ad1e715cba28
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-02-21 21:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (61.81%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool hasPrimeSetBits(int c){
+        if(c == 1)return false;
         if(c == 2 || c == 3 || c == 5 || c == 7 || c == 11 || c == 13 || c == 17 ||c == 19){
             return true;
         }else{
