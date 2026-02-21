cc9542bb8b5246b8ec9a480490b10d0a54609c24
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-02-21 21:58:25 +0530

    Time: 3 ms (72.17%) | Memory: 8 MB (61.81%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
-    bool hasPrimeSetBits(int n){
-        int c = 0;
-        while(n>0){
-            if(n%2 == 1)c++;
-            n /= 2;
-        }
+    bool hasPrimeSetBits(int c){
         if(c == 2 || c == 3 || c == 5 || c == 7 || c == 11 || c == 13 || c == 17 ||c == 19){
             return true;
         }else{
@@ -16,7 +11,7 @@ public:
     int countPrimeSetBits(int left, int right) {
         int count  = 0;
         for(int i = left;i<=right;i++){
-            if(hasPrimeSetBits(i))count++;
+            if(hasPrimeSetBits(__builtin_popcount(i)))count++;
         }
         return count;
     }
