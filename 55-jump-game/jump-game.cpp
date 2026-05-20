ae8248822ddf822ab822de882f8474d8fbceeb49
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-05-20 15:52:17 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (56.23%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,14 +4,10 @@ public:
         int maxReach = 0;
         int n = nums.size();
         for(int i = 0;i<n;i++){
-            if(i>maxReach){
-                return false;
-            }
+            if(i>maxReach)return false;
             maxReach = max(maxReach,i+nums[i]);
-            if(maxReach>n-1){
-                return true;
-            }
+            if(maxReach>=n-1)return true;
         }
-        return true;   
+        return true;
     }
 };
