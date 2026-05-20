ed127c26877fc4433d4be2b12b957642e2253999
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-05-20 15:04:36 +0530

    Time: 2 ms (76.65%) | Memory: 7.9 MB (38.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        static int count = 0;
-        if(n == 0){
-            return 0;
+        if(n<=1)return n;
+        int fibNum[n+1];
+        fibNum[0] = 0;
+        fibNum[1] = 1;
+        for(int i = 2;i<n+1;i++){
+            fibNum[i] = fibNum[i-1]+fibNum[i-2];
         }
-        else if( n == 1){
-            return 1;
-        }else{
-            count = fib(n-1)+fib(n-2);  
-        }
-        return count;
+        return fibNum[n];
     }
 };
