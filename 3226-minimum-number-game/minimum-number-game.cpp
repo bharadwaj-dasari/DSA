09f7721bcd321d98da89153a5e7530ee5696553c
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-05-26 22:06:34 +0530

    Time: 4 ms (12.02%) | Memory: 28.2 MB (51.03%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     vector<int> numberGame(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0 || n%2 != 0)return {0};
         sort(nums.begin(),nums.end());
-        for(int i = 0;i<nums.size();i=i+2){
+        for(int i = 0;i<n;i=i+2){
             swap(nums[i],nums[i+1]); 
         }
         return nums;
