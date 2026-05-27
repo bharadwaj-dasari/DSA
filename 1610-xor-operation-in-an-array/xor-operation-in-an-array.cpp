5b7e44003aca4fd61aa68da69859167c4b4066ed
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-05-27 15:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (78.22%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-        int res = start;
-        for(int i = 1;i<n;i++){
-            start += 2;
-            res ^= start;
+        int res = 0;
+        for(int i = 0;i<n;i++){
+            res ^= (start+2*i);
         }
         return res;
     }
