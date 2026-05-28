71dcd5817636b0b24b2e55287262901b985c8a56
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-05-28 20:55:02 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (80.75%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.cpp
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int result = 0;
-        for(const auto& row : accounts){
-            int cache = 0;
-            for(const auto& elements : row){
-                cache += elements;
+        int res = 0;
+        for(auto &row:accounts){
+            int c = 0;
+            for(auto &m:row){
+                c += m;
             }
-            if(cache > result)result = cache;
+            if(c>res)res = c;
         }
-        return result;
+        return res;
     }
 };
