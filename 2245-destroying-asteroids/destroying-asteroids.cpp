8241b8692e77cc4ec41350ee679f290b170c85db
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-05-31 11:43:03 +0530

    Time: 31 ms (66.42%) | Memory: 106.8 MB (16.20%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -3,9 +3,31 @@ public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         sort(asteroids.begin(),asteroids.end());
         long long int currMass = mass;
-        for(int n:asteroids){
-            if(currMass >= n)currMass += n;
-            else{return false;}
+        int count = 0;
+        for(int &n:asteroids){
+            if(currMass >= n)
+            {
+                currMass += n;
+                n = 0;
+            }
+            else
+            {
+                count++;
+            }
+        }
+        while(count>0){
+            int temp = 0;
+            for(int &k: asteroids){
+                if(currMass >= k){
+                    currMass += k;
+                    k = 0;
+                }
+                else{
+                    temp++;
+                }
+            }
+            if(count == temp)return false;
+            count = temp;
         }
         return true;
     }
