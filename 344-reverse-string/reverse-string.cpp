cfb26a72d093ca6889b950bec4868fe99a831bfd
Bharadwaj Naidu Dasari <167018078+bharadwaj-dasari@users.noreply.github.com>
2026-06-01 21:53:03 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (50.82%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -4,13 +4,7 @@ public:
         int i = 0;
         int j = s.size()-1;
         while(i<j){
-            if(s[i] != s[j]){
-                char t = s[i];
-                s[i] = s[j];
-                s[j] = t;
-            }
-            i++;
-            j--;
+           swap(s[i++],s[j--]); 
         }
     }
 };
